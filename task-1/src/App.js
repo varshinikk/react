d8ee9768100f03ddadf6aa1d7287a590ebe29f86
varshinkk <varshinishining@gmail.com>
2019-06-11 10:41:04 +0530

    task

---
+++ task-1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Signup from './Signup';
+import Signin from './Signin';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup/>
+      <Signin/>
+    </div>
+  );
+}
+
+export default App;
